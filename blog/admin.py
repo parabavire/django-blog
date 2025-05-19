3364dc13ab2f15bc3eaf1e4b7037b86d96135b5e
parabavire <jdparabavire@gmail.com>
2025-05-19 12:04:11 -0400

    ModelAdmin done

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Publication
 
 # Register your models here.
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'body')
+    search_fields = ('title', 'author__username', 'body')
+    list_filter = ('title', 'author__username')
+
+admin.site.register(Publication, PublicationAdmin)
