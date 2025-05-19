44791ce58442b3a068e3e03987dcede8ccf05520
parabavire <jdparabavire@gmail.com>
2025-05-19 12:41:33 -0400

    read model blog done

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Publication
 
 # Create your views here.
+class PublicationListView(ListView):
+    model = Publication
+    template_name = "publications-list.html"
