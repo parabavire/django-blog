8ea063ec28e7738be848317a7ab25ea459443e42
parabavire <jdparabavire@gmail.com>
2025-05-23 11:03:30 -0400

    permissions done

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 #from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django .views.generic.edit import (
+from django.views.generic.edit import (
                                         CreateView,
                                         UpdateView,
                                         DeleteView, #new
                                         ) 
 from .models import Publication
 from django.urls import reverse_lazy #new
+from django.contrib.auth.mixins import (
+                                        LoginRequiredMixin, #new
+                                        UserPassesTestMixin, #new
+                                        )
 
 # Create your views here.
 class PublicationListView(ListView):
@@ -20,14 +24,26 @@ class PublicationDetailView(DetailView):
 class PublicationCreateView(CreateView):
     model = Publication
     template_name = "publication-create.html"
-    fields = ['title', 'body', 'author']
+    fields = ['title', 'body']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class PublicationUpdateView(UpdateView):
+class PublicationUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Publication
     template_name = "publication-update.html"
     fields = ['title', 'body']
 
-class PublicationDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class PublicationDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Publication
     template_name = "publication-delete.html"
     success_url = reverse_lazy("publications-list") ###error
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
